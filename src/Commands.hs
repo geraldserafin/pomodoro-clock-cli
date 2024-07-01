002858d11c66441dbd78d900bda714243ec94eaf
geraldserafin <serafingerald@protonmail.com>
2024-07-01 14:08:35 +0200

    Add a separate type for deamon commands

--- src/Commands.hs
+++ src/Commands.hs
@@ -1,14 +1,25 @@
-module Commands (parseCommand, Command (Start, Status)) where
+{-# LANGUAGE DeriveGeneric #-}
+
+module Commands where
 
 import Options.Applicative
+import GHC.Generics (Generic)
+import Data.Aeson (ToJSON, FromJSON)
 
 data Command
+  = StartDeamon   
+  | MessageDeamon Message 
+
+data Message 
   = Start { workTime :: Int, breakTime :: Int, cycles :: Int }
   | Status
+  deriving (Show, Generic)
 
+instance ToJSON Message
+instance FromJSON Message
 
 startParser :: Parser Command
-startParser = Start
+startParser = MessageDeamon <$> (Start
   <$> option auto
       ( long    "work-time"
      <> short   'w'
@@ -31,27 +42,29 @@ startParser = Start
      <> value   1
      <> metavar "INT"
      <> help    "How many cycles to do" 
-      )
-
+      ))
 
 statusParser :: Parser Command
-statusParser = pure Status
+statusParser = pure $ MessageDeamon Status
 
+deamonCommandsParser :: Parser Command
+deamonCommandsParser = subparser 
+  ( command "start" (info (pure StartDeamon <**> helper) (progDesc "Starts the deamon"))
+  )
 
 commandParser :: Parser Command
 commandParser = subparser
-  ( command "start"  (info startParser  (progDesc "Start the timer"))
- <> command "status" (info statusParser (progDesc "Show the status"))
+  ( command "start"  (info (startParser  <**> helper) (progDesc "Start the timer"))
+ <> command "status" (info (statusParser <**> helper) (progDesc "Show the status"))
+ <> command "deamon" (info (deamonCommandsParser <**> helper) (progDesc "Manage the deamon"))
   )
-
 
 opts :: ParserInfo Command
 opts = info (commandParser <**> helper)
   ( fullDesc
  <> progDesc "A simple timer application"
  <> header "timer - a simple timer application" 
- )
-
+  )
 
 parseCommand :: IO Command
 parseCommand = execParser opts
--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,9 @@ import Commands
 
 main :: IO ()
 main = do
-  cmd <- parseCommand 
+  cmd <- parseCommand
 
   case cmd of
-    Status         -> putStrLn "Status: Timer is running"
-    Start wt bt cs -> putStrLn $ "Starting " 
-                              <> show cs <> " cycles of pomodoro with worktime: " 
-                              <> show wt <> " and breaktime: " 
-                              <> show bt
+    StartDeamon     -> putStrLn "Deamon Started"
+    MessageDeamon m -> print m
 
